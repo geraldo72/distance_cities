e8673115beb0dc0099c06ec297308897291f4144
geraldo72 <luis_geraldo72@hotmail.com>
2016-03-31 02:32:51 -0300

    Externalização das configuraçoes do MySQL

--- src/main/java/com/br/distanceCities/db/JdbcDAO.java
+++ src/main/java/com/br/distanceCities/db/JdbcDAO.java
@@ -1,28 +1,40 @@
 package com.br.distanceCities.db;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import com.br.distanceCities.exception.DatabaseException;
 
 public abstract class JdbcDAO {
 
-	private final String URL = "jdbc:mysql://localhost:3306";
-	private final String USER = "root";
-	private final String PASS = "teste";
+	private final String URL = "mysql.url";
+	private final String USER = "mysql.user";
+	private final String PASS = "mysql.pass";
 
 	protected final Connection getConnection() throws DatabaseException {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection(URL, USER, PASS);
+			
+			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+			Properties prop = new Properties();
+			prop.load(classloader.getResourceAsStream("connection.properties"));
+			
+			return DriverManager.getConnection(
+					prop.getProperty(URL, "jdbc:mysql://localhost:3306"), 
+					prop.getProperty(USER, "teste"),
+					prop.getProperty(PASS, "teste"));
 			
 		} catch (ClassNotFoundException e) {
 			throw new DatabaseException("Erro ao carregar o driver", e);
 		} catch (SQLException e) {
 			throw new DatabaseException("Erro ao conectar", e);
+		} catch (IOException e) {
+			throw new DatabaseException("Erro ao carregar arquivo de properties", e);
 		}
 	}
 	
